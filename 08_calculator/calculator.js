0552155cc20b224e30bdf121d4795563bdb3460f
Kiko Alquiros <kiksdoesstuff@gmail.com>
2023-06-15 10:18:19 -0400

    foundations pt.5 calculator done + extras

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	let add = x + y;
+  return add;
+  
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let sub = x - y;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function([...n]) {
+  let total = n.reduce((x, y) => x + y, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function([...n]) {
+  let total = n.reduce((x, y) => x * y, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let product = 1;
+  for (let i = 0; i < y; i++) {
+    product *= x;
+  };
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1;
+  for (let i = 0; i < x; i++) {
+    product *= (i + 1);
+  };
+  return product;
 };
 
 // Do not edit below this line
